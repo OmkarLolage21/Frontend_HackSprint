c8a7e7bb11cec7ccd900fc3898274eda1f870e14
Omkar Sunil Lolage <lolageomkar5@gmail.com>
2024-12-08 10:26:50 +0530

    updates layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,26 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
 import Header from './Header.js';
 import Sidebar from '../Layout/SideBar.js';
 
 const Layout = ({children}) => {
+
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+
+    Array.from(files).forEach((file) => {
+      setUploadedFiles((prevFiles) => [...prevFiles, file]);
+      setSelectedFile(file);
+    });
+  };
+
+  const onLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+  
   return (
     <div>
-        <Header/>
-        <span><main style={{minHeight:"80vh"}}>
-         {children};
-         <Sidebar/>
-         <div className="main-content">
+    <Header/>
+    <Sidebar/>
+
+    <div className="main-content">
+      <div className="uploaded-files">
+        <h2>Uploaded Files:</h2>
+        <ul>
+          {uploadedFiles.map((file, index) => (
+            <li key={index}>{file.name}</li>
+          ))}
+        </ul>
+      </div>
+
       <div className="upload-container">
         <label htmlFor="file-upload" className="upload-label">
           <div className="upload-area">
-            <p>Drag and drop files here or click to select</p>
-            <input type="file" id="file-upload" className="file-upload-input" />
+            <p>Drag and drop PDF files here or click to select</p>
+            <input
+              type="file"
+              id="file-upload"
+              className="file-upload-input"
+              onChange={handleFileChange}
+              accept=".pdf"
+              multiple
+            />
           </div>
         </label>
       </div>
+
+      {/* {selectedFile && (
+        <div className="file-content-container">
+          <h2>Selected PDF Content:</h2>
+          <div className="pdf-viewer">
+            <Document
+              file={URL.createObjectURL(selectedFile)}
+              onLoadSuccess={onLoadSuccess}
+            >
+              <Page pageNumber={pageNumber} />
+            </Document>
+            <p>
+              Page {pageNumber} of {numPages}
+            </p>
+          </div>
+        </div>
+      )} */}
     </div>
-        </main></span>
-        
+
     </div>
   );
 };
